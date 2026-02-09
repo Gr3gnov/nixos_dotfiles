9e915b055221deabcd4dc080c4d17b7a3e874458
Gr3gnov <use 82391045+Gr3gnov@users.noreply.github.com>
2026-02-09 22:30:42 +0200

    spectacle disable notification

--- home/modules/gui/plasma/spectacle.nix
+++ home/modules/gui/plasma/spectacle.nix
@@ -10,4 +10,23 @@
       immutable = true;
     };
   };
+
+  # Override Spectacle notification defaults to suppress popup toasts.
+  xdg.dataFile."knotifications6/spectacle.notifyrc".text = ''
+    [Global]
+    IconName=spectacle
+    DesktopEntry=org.kde.spectacle
+    Name=Spectacle
+    Comment=Screenshot Capture Utility
+
+    [Event/newScreenshotSaved]
+    Name=New Screenshot Saved
+    Comment=A new screenshot was captured and saved
+    Action=None
+
+    [Event/recordingSaved]
+    Name=Recording Saved
+    Comment=A recording was captured and saved
+    Action=None
+  '';
 }
